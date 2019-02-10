8d41f1b06a031ad91c6819f55c8835daed93a70b
Jaturan Sangmaneetip <jaturan.s@outlook.com>
2019-02-10 15:40:57 +0700

    study react rounter

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
 import './App.css'
 import React, { Component } from 'react';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Monitor from "./components/monitor/Monitor";
-
-
+import { BrowserRouter, Route, Swtich } from "react-router-dom";
+import Home from "./containers/Home";
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {products: ""};
-  }
-
-  componentDidMount(){
-    this.setState({products: [
-      { productId: 1, productName: "สลัดผัก", unitPrice: "120", thumbnail: "/images/product/1.jpg" },
-      { productId: 2, productName: "ไก่ทอด", unitPrice: "90", thumbnail: "/images/product/2.jpg" },
-      { productId: 3, productName: "บิงซู", unitPrice: "200", thumbnail: "/images/product/3.jpg" },
-      { productId: 4, productName: "เฟรนฟราย", unitPrice: "140", thumbnail: "/images/product/4.jpg" },
-      { productId: 5, productName: "เค้ก 3 ชั้น", unitPrice: "200", thumbnail: "/images/product/5.jpg" },
-      { productId: 6, productName: "กาแฟ เฮลตี้ฟู้ด", unitPrice: "140", thumbnail: "/images/product/6.jpg" }
-     ]});
+  renderRouter() {
+    return (
+      <Switch>
+        <Route path="/" Component={Home} />
+      </Switch>
+    )
   }
 
   render() {
     return (
-      <div>
-        <Header />
-        <Monitor products={this.state.products} />
-        <Footer company="JATURANCO" email="jaturan.s@gmail.com" />
-      </div>
+      <BrowserRouter> {this.renderRouter()} </BrowserRouter>
     );
   }
 }
